0fe1f9a9cfca5b30a7977321df7f41bb194d9db0
you <dolla_@naver.com>
2022-03-27 13:11:50 +0900

    Feat : ItemEntity(add deliveryMethod => direct/post)

--- src/main/java/com/project/market/modules/delivery/entity/DeliveryMethod.java
+++
@@ -1,6 +0,0 @@
-package com.project.market.modules.delivery.entity;
-
-public enum DeliveryMethod {
-    //직거래, 택배거래
-    DIRECT, PARCEL, POST
-}
---
+++ src/main/java/com/project/market/modules/item/validator/ItemFormValidator.java
@@ -0,0 +1,2 @@
+package com.project.market.modules.item.validator;public class ItemFormValidatory {
+}
